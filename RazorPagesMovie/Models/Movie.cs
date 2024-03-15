f6f415562b58644630227078e0c5b567e816b344
wadariel <aidar.asanov.99@gmail.com>
2024-03-15 18:53:12 +0300

    Initial commit

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Название")]
+        public string? Title { get; set; }
+
+        [Display(Name = "Дата выхода")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Жанр")]
+        public string? Genre {  get; set; }
+
+        [Display(Name = "Стоимость")]
+        [Column(TypeName = "decimal(18,2")]
+        public decimal Price { get; set; }
+    }
+}
